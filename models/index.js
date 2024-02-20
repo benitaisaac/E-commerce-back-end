5e52e7af0716937965cc9139edbee51b679a2ef9
benitaisaac <benita.isaac@gmail.com>
2024-02-19 18:24:17 -0800

    fixed bug in belongsToMany method

--- models/index.js
+++ models/index.js
@@ -13,14 +13,14 @@ Category.hasMany(Product, {
 });
 
 // Products belongToMany Tags (through ProductTag)
-Product.belongToMany(Tag, {
+Product.belongsToMany(Tag, {
   through: {
     model: ProductTag
   }
 });
 
 // Tags belongToMany Products (through ProductTag)
-Tag.belongToMany(Product, {
+Tag.belongsToMany(Product, {
   through: {
     model: ProductTag
   },
