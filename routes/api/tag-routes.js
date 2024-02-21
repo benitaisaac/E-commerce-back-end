c751f0684784a4ec6a71dfed97b86ac80c7cf230
benitaisaac <benita.isaac@gmail.com>
2024-02-20 16:33:36 -0800

    added get route for a specific id

--- routes/api/tag-routes.js
+++ routes/api/tag-routes.js
@@ -34,8 +34,15 @@ router.get("/:id", async (req, res) => {
   }
 });
 
-router.post("/", (req, res) => {
-  // create a new tag
+// create a new tag
+router.post("/", async (req, res) => {
+  try{
+    const newTag = await Tag.create(req.body);
+    res.status(200).json(newTag)
+
+  } catch {
+
+  }
 });
 
 router.put("/:id", (req, res) => {
