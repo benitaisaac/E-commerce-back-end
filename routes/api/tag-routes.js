96eebde38312a2d62ec8c0dc2600df2e799b45a5
benitaisaac <benita.isaac@gmail.com>
2024-02-20 16:24:04 -0800

    added get route for specific tag id

--- routes/api/tag-routes.js
+++ routes/api/tag-routes.js
@@ -1,37 +1,48 @@
-const router = require('express').Router();
-const { Tag, Product, ProductTag } = require('../../models');
+const router = require("express").Router();
+const { Tag, Product, ProductTag } = require("../../models");
 
 // The `/api/tags` endpoint
 
-
-  // find all tags
-  // be sure to include its associated Product data
-router.get('/', async (req, res) => {
+// find all tags
+// be sure to include its associated Product data
+router.get("/", async (req, res) => {
   try {
     const getTags = await Tag.findAll({
-      include: [{model: Product, through: ProductTag}]
+      include: [{ model: Product, through: ProductTag }],
     });
-    console.log(getTags);
     res.status(200).json(getTags);
-  } catch(err) {
+  } catch (err) {
     res.status(500).json(err);
   }
 });
 
-router.get('/:id', (req, res) => {
-  // find a single tag by its `id`
-  // be sure to include its associated Product data
+// find a single tag by its `id`
+// be sure to include its associated Product data
+router.get("/:id", async (req, res) => {
+  try {
+    const tagId = req.params.id
+    const tagData = await Tag.findByPk(tagId, {
+      include: [{model: Product, through: ProductTag }],
+    });
+    if(!tagData) {
+      res.status(404).json({message: "no category found with this id!"});
+      return;
+    }
+    res.status(200).json(tagData);
+  } catch(err) {
+    res.status(500).json(err)
+  }
 });
 
-router.post('/', (req, res) => {
+router.post("/", (req, res) => {
   // create a new tag
 });
 
-router.put('/:id', (req, res) => {
+router.put("/:id", (req, res) => {
   // update a tag's name by its `id` value
 });
 
-router.delete('/:id', (req, res) => {
+router.delete("/:id", (req, res) => {
   // delete on tag by its `id` value
 });
 
