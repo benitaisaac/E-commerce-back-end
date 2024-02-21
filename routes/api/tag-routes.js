af7b6983eb6a3ef542e57e616511fdfabfe8a8fe
benitaisaac <benita.isaac@gmail.com>
2024-02-20 16:37:56 -0800

    added post route under tag to make a new tag

--- routes/api/tag-routes.js
+++ routes/api/tag-routes.js
@@ -38,10 +38,9 @@ router.get("/:id", async (req, res) => {
 router.post("/", async (req, res) => {
   try{
     const newTag = await Tag.create(req.body);
-    res.status(200).json(newTag)
-
-  } catch {
-
+    res.status(200).json(newTag);
+  } catch(err) {
+    res.status(400).json(err);
   }
 });
 
